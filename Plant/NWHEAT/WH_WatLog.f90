6d2229beb7c66a43a6930db436b0da46293e1a90
vshelia <errtovsh@gmail.com>
2020-04-28 15:24:12 -0400

    New Module for Waterlogging
    New Module for Waterlogging variables, functions, etc.

--- Plant/NWHEAT/WH_WatLog.f90
+++ Plant/NWHEAT/WH_WatLog.f90
@@ -1,7 +1,8 @@
 Module WatLog
     Implicit None
     
-    
-    
-    
+    Character(Len=12) :: OUTWL = 'WatLog.OUT'
+    Integer :: NOUTWL
+    Real, dimension(20) :: fdsw_test
 End Module WatLog
+    
